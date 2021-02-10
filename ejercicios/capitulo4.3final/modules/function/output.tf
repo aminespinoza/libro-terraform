155b48dde8986b4a89feab28b364c3f7a8b21358
Amin Espinoza <doomsday5@hotmail.com>
2021-02-10 08:42:13 -0600

    capitulo4.3 agregado

--- ejercicios/capitulo4.3final/modules/function/output.tf
+++ ejercicios/capitulo4.3final/modules/function/output.tf
@@ -1,11 +1,11 @@
-output "function_name" {
+output "app_function_name" {
   value = azurerm_function_app.azure_function.name
 }
 
-output "storage_name" {
+output "storage_account_name" {
   value = azurerm_storage_account.storage.name
 }
 
-output "app_service_plan" {
+output "app_service_plan_name" {
   value = azurerm_app_service_plan.service_plan.name
 }
